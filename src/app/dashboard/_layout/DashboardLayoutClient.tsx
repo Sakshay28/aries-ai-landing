466f24b9315c35ed84e19affa6287c9683694323
Sakshay-28 <asty26978@gmail.com>
2026-05-22 23:56:47 +0530

    Revert "fix: move overflow-auto off main element to prevent stacking context trapping portal dropdowns"
    This reverts commit ce8d0365f325d7dc883fab79aaaf737d9db871f1.

--- src/app/dashboard/_layout/DashboardLayoutClient.tsx
+++ src/app/dashboard/_layout/DashboardLayoutClient.tsx
@@ -20,8 +20,8 @@ export default function DashboardLayoutClient({
         <Backdrop />
         <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
           <AppHeader />
-          <main className="flex-1 overflow-hidden">
-            <div className="w-full h-full overflow-auto p-4 md:p-6 lg:p-8">{children}</div>
+          <main className="flex-1 overflow-auto">
+            <div className="w-full p-4 md:p-6 lg:p-8">{children}</div>
           </main>
         </div>
       </div>
